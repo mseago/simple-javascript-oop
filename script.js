d2cfa11876d7a6ca4821c61ef59c8afd4e6b5d94
Melissa Seago <melissa.seago@gmail.com>
2016-07-10 15:47:00 -0500

    improved

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
 'use strict';
 
-function Car() {
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+  this.passengers = [];
+}
+Car.prototype = {
+  honk: function() {
+    return 'beep beep';
+  },
+  addPassenger: function(passenger) {
+    this.passengers.push(passenger);
+  },
+  description: function() {
+    return 'This is a ' + this.make + ' ' + this.model;
+  },
+  passengerList: function() {
+    var self = this;
+    if (this.passengers.length === 0) {
+      return 'no passengers';
+  } else if (this.passengers.length === 1) {
+      return this.passengers[0];
+  } else if (this.passengers.length > 2) {
+    var allPassengers = '';
+    self.passengers.forEach(function(passenger, idx) {
+      if (idx === self.passengers.length - 1) {
+        allPassengers += 'and ' + passenger;
+      } else {
+        allPassengers += passenger + ', ';
+      }
+    });
+    return allPassengers;
+    }
+  }
+
+}
+
+function ChevyCar(){}
+ChevyCar.prototype = new Car();
+ChevyCar.prototype.breakDown = function(){}
+ChevyCar.prototype.honk = function() {
+  return 'boop boop';
 }
